6284b5e0313bdf2b7722b2e38640d9bf46b4d15f
Dhaya Benmessaoud <d.benmessaoud@oodrive.fr>
2018-06-01 12:02:26 +0200

    Load background script with actual config

--- background.js
+++ background.js
@@ -3,14 +3,30 @@ const baseUrl = 'https://giphy.com/search/';
 const GIPHY_KEY = '';
 const GIPHY_API = 'https://api.giphy.com';
 
-let DELAY = 0.1;
-chrome.alarms.create("alarma", { delayInMinutes: DELAY, periodInMinutes: DELAY });
-chrome.alarms.get("alarma", (alarm) => console.log(alarm));
+const createAlarm = (delay) => chrome.alarms.create('alarma', { delayInMinutes: delay, periodInMinutes: delay });
+
+let currentConfig;
+config.load().then((config) => {
+	currentConfig = config;
+	createAlarm(config.delay);
+});
 
 const getRandomGif = () => {
-	let apiPromise = fetch(`${GIPHY_API}/v1/gifs/random?api_key=${GIPHY_KEY}`)
+	const apiUrl = currentConfig.api === 'random'
+		? `${GIPHY_API}/v1/gifs/random?api_key=${GIPHY_KEY}`
+		: `${GIPHY_API}/v1/gifs/search?api_key=${GIPHY_KEY}&q=${currentConfig.keywords}`
+	;
+
+	let apiPromise = fetch(apiUrl)
 		.then((response) => response.json())
-		.then((json) => json.data.images.original.url)
+		.then((json) => {
+			if (Array.isArray(json.data)) {
+				const index = Math.floor(Math.random() * json.data.length);
+				return json.data[index].images.original.url;
+			}
+
+			return json.data.images.original.url;
+		})
 	;
 	apiPromise.then((gifUrl) => {
 		chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
